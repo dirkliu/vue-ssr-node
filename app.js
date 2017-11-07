5c38688de2ea22fc48335701560ed401e09dfce8
dirkliu <liuqi0702@126.com>
2017-11-07 23:31:55 +0800

    node server render!

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
+// 第 1 步：创建一个 Vue 实例
 const Vue = require('vue')
+
+// 第 2 步：创建一个 renderer
+const renderer = require('vue-server-renderer').createRenderer()
+// 第 3 步：将 Vue 实例渲染为 HTML
+
 const server = require('express')()
-const renderer = require('vue-server-renderer').createRenderer();
 
 server.get('*', (req, res) => {
   const app = new Vue({
-    data () {
-      return {
-        url: req.url,
-        template: `<div>访问的 URL 是： {{ url }}</div>`
-      }
-    }
+    data: {
+      url: req.url
+    },
+    template: `<div>访问的 URL 是： {{ url }}</div>`
   })
 
   renderer.renderToString(app, (err, html) => {
     if (err) {
-      res.status(500).end('Internal Server Error: ' + err)
+      res.status(500).end('Internal Server Error')
       return
     }
-
     res.end(`
       <!DOCTYPE html>
       <html lang="en">
-        <head><title>Hello</title></head>
-        <body>test</body>
+        <head><meta charset="utf-8"><title>Hello</title></head>
+        <body>${html}</body>
       </html>
-      `)
+    `)
   })
 })
 server.listen(8080)
