0f002424eb5fd89c79bb6e3e227925e802916ac4
刘琦 <liuqi1-iri@360.cn>
2017-11-24 17:13:35 +0800

    client 与

--- app.js
+++ app.js
@@ -1,35 +1,10 @@
-// 第 1 步：创建一个 Vue 实例
 const Vue = require('vue')
-
-// 第 2 步：创建一个 renderer
-const renderer = require('vue-server-renderer').createRenderer({
-  template: require('fs').readFileSync('./index.template.html', 'utf-8')
-})
-// 第 3 步：将 Vue 实例渲染为 HTML
-
-const server = require('express')()
-const context = {
-  title: 'vue ssr demo',
-  meta: `
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  `
-}
-
-server.get('*', (req, res) => {
-  const app = new Vue({
+module.exports = function createApp (context) {
+  console.log('context:', context)
+  return new Vue({
     data: {
-      url: req.url
+      url: context.url
     },
     template: `<div>访问的 URL 是： {{ url }}</div>`
   })
-
-  renderer.renderToString(app, context, (err, html) => {
-    if (err) {
-      res.status(500).end('Internal Server Error')
-      return
-    }
-    res.end(html)
-  })
-})
-server.listen(8080)
+}
